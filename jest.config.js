f3e8fb1f5c6972bb2c3f8719ee26b0d075bc627d
Hannah Walsh <hannah.walsh@credera.com>
2024-03-06 12:58:40 -0600

    Fix jest testing configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '.(css|less|scss)$': 'identity-obj-proxy',
+    "^.+\\.svg$": "jest-svg-transformer"
+  }
 };
